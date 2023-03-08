81e91f1e89aaee39d1f7b84dc9c8ba26ba586d0b
fwuu0017 <fwuu0017@student.monash.edu>
2023-03-08 23:30:07 +0800

    create the initial spring boot project structure, set up the basic configurations including spring web, mysql, JPA

---
+++ src/main/java/com/recycling/config/JPAConfig.java
@@ -0,0 +1,27 @@
+package com.recycling.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class JPAConfig {
+
+    @Bean
+    public DataSource dataSource() {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        dataSource.setUrl(
+                "jdbc:mysql://localhost:3306/imc_db?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC");
+
+        return dataSource;
+    }
+
+}
