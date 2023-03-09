c074cfa117c9e0bf129cb5bb5637e924e3e1c47a
fwuu0017 <fwuu0017@student.monash.edu>
2023-03-10 02:41:12 +0800

    build up the geo spatial utils with shape query function, test on the demo application,

--- src/main/java/com/recycling/DemoApplication.java
+++ src/main/java/com/recycling/DemoApplication.java
@@ -1,13 +1,37 @@
 package com.recycling;
 
+import com.recycling.untils.GeoSpatialUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.*;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) throws IOException {
+//        SpringApplication.run(DemoApplication.class, args);
+
+        // test for geo query
+        Resource resource = new ClassPathResource("static/location.json");
+        InputStream is = resource.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String data = null;
+        String jsonFile = null;
+        while((data = br.readLine()) != null) {
+            jsonFile = data;
+        }
+
+        br.close();
+        isr.close();
+        is.close();
+
+        boolean result = GeoSpatialUtils.geoShapeQuery(jsonFile,147.2600,-36.8595);
+
+        System.out.println(result);
 
     }
 
