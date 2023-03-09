8df4d0f5925a94a45b64c8a1f4fd7586f3768988
fwuu0017 <fwuu0017@student.monash.edu>
2023-03-10 02:41:33 +0800

    build up the geo spatial utils with shape query function, test on the demo application,

---
+++ src/main/java/com/recycling/untils/GeoSpatialUtils.java
@@ -0,0 +1,110 @@
+package com.recycling.untils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Polygon;
+
+import java.util.List;
+
+public class GeoSpatialUtils {
+
+    public static boolean geoShapeQuery(String geoJsonString, double lng, double lat) {
+        boolean result = false;
+
+        Coordinate coordinate = new Coordinate(lng,lat);
+        List<String> multiPolygonCoordinates = readCoordinatesList(geoJsonString);
+
+        for (String polygonCoordinates : multiPolygonCoordinates) {
+
+            String[] multiRingCoordinates = polygonCoordinates
+                    .replace("]],", "]]*")
+                    .replace("[[[","[[").replace("]]]","]]")
+                    .split("[*]");
+
+
+            String[] outerRingCoordinates = ringFormatter(multiRingCoordinates[0]);
+            Coordinate[] outerCoordinateArray = stringToCoordinate(outerRingCoordinates);
+
+            if (pointWithinPolygon(outerCoordinateArray, coordinate)) {
+                result = true;
+
+                for (int i = 1; i < multiRingCoordinates.length; i++) {
+                    String[] innerRingCoordinates = ringFormatter(multiRingCoordinates[i]);
+                    Coordinate[] innerCoordinateArray = stringToCoordinate(innerRingCoordinates);
+
+                    if (pointWithinPolygon(innerCoordinateArray, coordinate)) {
+                        result = false;
+                        break;
+                    }
+
+                }
+
+            }
+
+        }
+
+        return result;
+    }
+
+    private static List<String> readCoordinatesList(String geoJsonString) {
+        JSONObject jsonObject = JSONObject.parseObject(geoJsonString);
+        String geometry = jsonObject.getString("geometry");
+
+        return JSON.parseArray(
+                JSONObject.parseObject(geometry).getJSONArray("coordinates").toJSONString(),
+                String.class
+        );
+
+    }
+
+    private static Coordinate[] stringToCoordinate(String[] ringCoordinates) {
+
+        Coordinate[] coordinates = new Coordinate[ringCoordinates.length];
+
+        for (int i = 0; i < ringCoordinates.length; i++) {
+            String[] lngLat = ringCoordinates[i].replace("[","").replace("]","").split(",");
+
+            Coordinate coordinate = new Coordinate(Double.parseDouble(lngLat[0]), Double.parseDouble(lngLat[1]));
+            coordinates[i] = coordinate;
+        }
+
+        return coordinates;
+
+    }
+
+    private static String[] ringFormatter(String ringCoordinates) {
+
+        return ringCoordinates
+                .replace("],", "]*")
+                .replace("[[","[").replace("]]","]")
+                .split("[*]");
+
+    }
+
+    private static boolean pointWithinPolygon(Coordinate[] coordinateArray, Coordinate coordinate) {
+
+        boolean result = false;
+
+        if (coordinateArray.length < 3) {
+            return false;
+        }
+
+        if (!(coordinateArray[0].equals2D(coordinateArray[coordinateArray.length - 1]))) {
+            return false;
+        }
+
+        GeometryFactory geometryFactory = new GeometryFactory();
+        Point point = geometryFactory.createPoint(coordinate);
+        Polygon polygon = geometryFactory.createPolygon(coordinateArray);
+
+        if (point.within(polygon) || point.intersects(polygon)) {
+            result = true;
+        }
+
+        return result;
+    }
+
+}
---
+++ target/classes/WEB-INF/hello.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Recycling Management System</title>
+</head>
+<body>
+    <h1>This is our Home page</h1>
+</body>
+</html>
