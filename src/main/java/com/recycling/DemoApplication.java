b976c042831ab099a16c6aef4da934c4fbd4d836
fwuu0017 <fwuu0017@student.monash.edu>
2023-03-14 04:33:33 +0800

    optimize search speed, pre-process data before application runs

--- src/main/java/com/recycling/DemoApplication.java
+++ src/main/java/com/recycling/DemoApplication.java
@@ -1,14 +1,45 @@
 package com.recycling;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
 
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static List<String> features;
+
+    public static void main(String[] args) throws IOException {
+
+        Resource resource = new ClassPathResource("static/geo_json.json");
+        InputStream is = resource.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String data = null;
+        String jsonFile = null;
+        while((data = br.readLine()) != null) {
+            jsonFile = data;
+        }
+
+        br.close();
+        isr.close();
+        is.close();
+
+        JSONObject jsonObject = JSONObject.parseObject(jsonFile);
+        features = JSON.parseArray(jsonObject.getJSONArray("features").toJSONString(), String.class);
+
         SpringApplication.run(DemoApplication.class, args);
+
     }
 
 }
--- src/main/java/com/recycling/service/GeoSpatialService.java
+++ src/main/java/com/recycling/service/GeoSpatialService.java
@@ -7,14 +7,11 @@ import com.byteowls.jopencage.model.JOpenCageForwardRequest;
 import com.byteowls.jopencage.model.JOpenCageLatLng;
 import com.byteowls.jopencage.model.JOpenCageResponse;
 import com.recycling.untils.GeoSpatialUtils;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
+
+import static com.recycling.DemoApplication.features;
 
 public class GeoSpatialService {
 
@@ -38,28 +35,6 @@ public class GeoSpatialService {
     }
 
     public String geoShapeQuery(Double lng, Double lat) throws IOException {
-//        System.out.println(System.currentTimeMillis());
-
-        Resource resource = new ClassPathResource("static/geo_json.json");
-        InputStream is = resource.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        String data = null;
-        String jsonFile = null;
-        while((data = br.readLine()) != null) {
-            jsonFile = data;
-        }
-
-        br.close();
-        isr.close();
-        is.close();
-
-//        System.out.println(System.currentTimeMillis());
-
-        JSONObject jsonObject = JSONObject.parseObject(jsonFile);
-        List<String> features = JSON.parseArray(jsonObject.getJSONArray("features").toJSONString(), String.class);
-
-//        System.out.println(System.currentTimeMillis());
 
         for (String feature : features) {
 
@@ -70,13 +45,9 @@ public class GeoSpatialService {
 
             if (GeoSpatialUtils.geoShapeQuery(coordinates,lng,lat)) {
 
-//                System.out.println(System.currentTimeMillis());
-
                 return GeoSpatialUtils.getLgaId(feature);
             }
         }
-
-//                System.out.println(System.currentTimeMillis());
 
         return "-1";
     }
